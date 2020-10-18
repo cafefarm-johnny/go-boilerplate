70cb1407847c1cef6eb635676f979e98a274b43f
cafefarm-johnny <npcdja@gmail.com>
2020-10-18 17:27:29 +0900

    build: 앱 실행 시 profile 환경변수 검증 과정 추가

--- server/startup.go
+++ server/startup.go
@@ -1,16 +1,22 @@
 package server
 
 import (
+	"fmt"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"go-boilerplate/domain/route"
 	customError "go-boilerplate/server/configure/error"
 	"log"
+	"os"
 )
 
 const (
 	port = ":8080"
 )
+
+func init() {
+	validate()
+}
 
 func Startup() {
 	e := echo.New()
@@ -20,4 +26,23 @@ func Startup() {
 	route.Register(e)
 
 	log.Fatal(e.Start(port))
+}
+
+func validate() {
+	valid := false
+
+	profile := os.Getenv("profile")
+
+	switch profile {
+	case "local", "dev", "stage", "prod":
+		valid = !valid
+		break
+	default:
+		break
+	}
+
+	if !valid {
+		fmt.Println("Startup Failed. Cause: 'profile' environment variable is nil.")
+		os.Exit(1)
+	}
 }
